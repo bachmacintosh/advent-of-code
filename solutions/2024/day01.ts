0295d8750bfad632a2ec1e5d1f2fdfa6b1f2df4e
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2024-11-29 15:16:46 -0500

    Initial Commit
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

---
+++ solutions/2024/day01.ts
@@ -0,0 +1,10 @@
+import type { AdventDay } from "../../types.ts";
+
+const day01: AdventDay = {
+  year: 2024,
+  day: 1,
+  part1: (input) => input,
+  part2: (input) => input,
+};
+
+export default day01;
---
+++ solutions/2024/index.ts
@@ -0,0 +1,8 @@
+import type { AdventDay } from "../../types.ts";
+import day01 from "./day01.ts";
+
+const year2024: AdventDay[] = [
+  day01,
+] as const;
+
+export default year2024;
