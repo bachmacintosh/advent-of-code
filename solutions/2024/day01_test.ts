a8577f80f8295a41cc8679123104717e1860d5ce
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2024-12-01 12:23:29 -0500

    Day 1: Historian Hysteria
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- solutions/2024/day01_test.ts
+++ solutions/2024/day01_test.ts
@@ -2,8 +2,13 @@ import day01 from "./day01.ts";
 import { expect } from "@std/expect";
 
 Deno.test("Year 2024 Day 1 Part 1", () => {
-  const input = "Hello World!";
-  expect(day01.part1(input)).toBe(input);
+  const input = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
+  expect(day01.part1(input)).toBe(11);
 });
 
 Deno.test("Year 2024 Day 1 Part 2", () => {
