7940a73bf1172e271b57f725ae43962489bfef08
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2024-12-01 14:33:10 -0500

    Day 1: Historian Hysteria (#3)
    * Day 1: Historian Hysteria
    
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
    
    * Part 1
    
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
    
    * Part 2
    
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
    
    * Better tests
    
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
    
    * Export location list function and remove unreachable error
    
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- solutions/2024/day01_test.ts
+++ solutions/2024/day01_test.ts
@@ -1,12 +1,25 @@
-import day01 from "./day01.ts";
+import { describe, test } from "@std/testing/bdd";
+import day01, { getLocationLists } from "./day01.ts";
 import { expect } from "@std/expect";
 
-Deno.test("Year 2024 Day 1 Part 1", () => {
-  const input = "Hello World!";
-  expect(day01.part1(input)).toBe(input);
-});
+const input = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
 
-Deno.test("Year 2024 Day 1 Part 2", () => {
-  const input = "Hello World!";
-  expect(day01.part2(input)).toBe(input);
+describe("Year 2024 Day 1", () => {
+  test("getLocationLists turns the input list into 2 Location ID lists", () => {
+    expect(getLocationLists(input)).toEqual([[3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3]]);
+  });
+  test("getLocationLists throws an error when invalid data is provided", () => {
+    expect(() => getLocationLists(`nope   lol`)).toThrow();
+  });
+  test("Part 1 returns a sum of the distances between the sorted numbers on the left and right lists", () => {
+    expect(day01.part1(input)).toBe(11);
+  });
+  test("Part 2 returns the summed Similarity Scores (how many times a numbeer on the left appears on the right) of all numbers in the left list", () => {
+    expect(day01.part2(input)).toBe(31);
+  });
 });
