cb5fd3427a7d247881fc7c5478af611afe4b0724
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2024-12-02 14:00:11 -0500

    Part 1
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- solutions/2024/day02.ts
+++ solutions/2024/day02.ts
@@ -3,8 +3,44 @@ import type { AdventDay } from "../../types.ts";
 const day02: AdventDay = {
   year: 2024,
   day: 2,
-  part1: (_input): number => 0,
+  part1: (input): number => {
+    const reports = getReports(input);
+    let safeReports = 0;
+    reportLoop: for (const report of reports) {
+      if (report.length < 2) {
+        throw new Error("Not enough data to determine if Report is safe based on criteria");
+      }
+      const increasing = report[1] - report[0] > 0;
+      const decreasing = report[1] - report[0] < 0;
+      for (const [levelIdx] of report.entries()) {
+        if (levelIdx >= 1) {
+          const delta = report[levelIdx] - report[levelIdx - 1];
+          if (
+            (Math.abs(delta) < 1 || Math.abs(delta) > 3) || (increasing && delta <= 0) || (decreasing && delta >= 0)
+          ) {
+            continue reportLoop;
+          }
+        }
+      }
+      safeReports += 1;
+    }
+    return safeReports;
+  },
   part2: (_input) => null,
 };
+
+export function getReports(input: string): number[][] {
+  const reports = input.split("\n").filter((line) => line.length).map((line) =>
+    line.split(" ").map((item) => Number(item))
+  );
+  reports.forEach((report, reportIdx) => {
+    report.forEach((level, levelIdx) => {
+      if (isNaN(level)) {
+        throw new TypeError(`Invalid numerical data in report, row ${reportIdx} column ${levelIdx}`);
+      }
+    });
+  });
+  return reports;
+}
 
 export default day02;
--- solutions/2024/day02_test.ts
+++ solutions/2024/day02_test.ts
@@ -1,5 +1,5 @@
 import { describe, test } from "@std/testing/bdd";
-import day02 from "./day02.ts";
+import day02, { getReports } from "./day02.ts";
 import { expect } from "@std/expect";
 
 const input = `7 6 4 2 1
@@ -10,7 +10,22 @@ const input = `7 6 4 2 1
 1 3 6 7 9`;
 
 describe("Year 2024 Day 2", () => {
+  test("getReports turns the input list into an array of number arrays", () => {
+    expect(getReports(input)).toEqual([[7, 6, 4, 2, 1], [1, 2, 7, 8, 9], [9, 7, 6, 2, 1], [1, 3, 2, 4, 5], [
+      8,
+      6,
+      4,
+      4,
+      1,
+    ], [1, 3, 6, 7, 9]]);
+  });
+  test("getReports throws an error with invalid numerical data", () => {
+    expect(() => getReports(`1 2 lol`)).toThrow();
+  });
   test("Part 1 returns number of safe reports based on their level data", () => {
     expect(day02.part1(input)).toBe(2);
+  });
+  test("Part 1 throws an error when there isn't enough data in a report to determine if it's safe", () => {
+    expect(() => day02.part1(`1 2 3\n4`)).toThrow();
   });
 });
