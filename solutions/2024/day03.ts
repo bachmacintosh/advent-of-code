2e0ac49ab6a28c4f147e497e1eed9acd18faeffb
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2024-12-03 12:01:44 -0500

    Part 1
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- solutions/2024/day03.ts
+++ solutions/2024/day03.ts
@@ -1,9 +1,19 @@
 import type { AdventDay } from "../../types.ts";
 
+const MUL_REGEX = /mul\((?<num1>\d{1,3}),(?<num2>\d{1,3})\)/gu;
+
 const day03: AdventDay = {
   year: 2024,
   day: 3,
-  part1: () => 0,
+  part1: (input) => {
+    let sum = 0;
+    for (const match of input.matchAll(MUL_REGEX)) {
+      const num1 = Number(match.groups?.num1);
+      const num2 = Number(match.groups?.num2);
+      sum += num1 * num2;
+    }
+    return sum;
+  },
   part2: () => 0,
 };
 
