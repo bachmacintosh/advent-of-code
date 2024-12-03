d4d01c6d926c5b0ad26316778c69df00142e328d
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2024-12-03 15:02:52 -0500

    Day 3: Mull It Over (#5)
    * Day 3: Mull It Over
    
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
    
    * Part 1
    
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
    
    * Part 2
    
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- solutions/2024/index.ts
+++ solutions/2024/index.ts
@@ -1,10 +1,12 @@
 import type { AdventDay } from "../../types.ts";
 import day01 from "./day01.ts";
 import day02 from "./day02.ts";
+import day03 from "./day03.ts";
 
 const year2024: AdventDay[] = [
   day01,
   day02,
+  day03,
 ] as const;
 
 export default year2024;
